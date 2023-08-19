1cc61c41c1788ea9c9345b1bf0b1b17c1439b05d
deni zakya <denizakya@gmail.com>
2023-08-19 21:33:21 +0200

    add /guess/{number} path

--- Sources/SwiftEdgeApp/App.swift
+++ Sources/SwiftEdgeApp/App.swift
@@ -4,7 +4,20 @@ import Compute
 struct App {
 
     static func main() async throws {
-        try await onIncomingRequest(handler)
+//         try await onIncomingRequest(handler)
+        let router = Router()
+
+        router.get("/guess") { req, res in
+            let number = Int(req.url.lastPathComponent) ?? 0
+            let answer = (1...10).randomElement() ?? 0
+            if number == answer {
+                try await res.status(.ok).send("You guess the right number")
+            } else {
+                try await res.status(.ok).send("You guess \(number) but the answer is \(answer)")
+            }
+        }
+
+        try await router.listen()
     }
 
     static func handler(
