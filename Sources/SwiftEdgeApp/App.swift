e7d54e4149c887dd20629b2507a24e6a4da764c0
deni zakya <denizakya@gmail.com>
2023-08-19 21:42:27 +0200

    Use onIncomingRequest

--- Sources/SwiftEdgeApp/App.swift
+++ Sources/SwiftEdgeApp/App.swift
@@ -4,26 +4,19 @@ import Compute
 struct App {
 
     static func main() async throws {
-//         try await onIncomingRequest(handler)
-        let router = Router()
-
-        router.get("/guess") { req, res in
-            let number = Int(req.url.lastPathComponent) ?? 0
-            let answer = (1...10).randomElement() ?? 0
-            if number == answer {
-                try await res.status(.ok).send("You guess the right number")
-            } else {
-                try await res.status(.ok).send("You guess \(number) but the answer is \(answer)")
-            }
-        }
-
-        try await router.listen()
+         try await onIncomingRequest(handler)
     }
 
     static func handler(
         req: IncomingRequest,
         res: OutgoingResponse
     ) async throws {
-        try await res.status(.ok).send("Hello, Swift.")
+        let number = Int(req.url.lastPathComponent) ?? 0
+        let answer = (1...10).randomElement() ?? 0
+        if number == answer {
+            try await res.status(.ok).send("You guess the right number")
+        } else {
+            try await res.status(.ok).send("You guess \(number) but the answer is \(answer)")
+        }
     }
 }
